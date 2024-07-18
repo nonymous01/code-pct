18148c9cc31fdcc105b26a3b77f759ce7d34cb9a
nonymous01 <youssef.anonymous0@gmail.com>
2024-07-18 21:48:44 +0000

    e

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js'],
+  darkMode: false, // ou 'media' ou 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
